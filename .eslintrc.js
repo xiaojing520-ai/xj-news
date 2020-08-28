ca74018f20ea58340806e8d5d415a7ae38f33a82
xiaojing520-ai <542258713@qq.com>
2020-08-28 09:46:23 +0800

    初始化提交

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // key: 规则的名字  off: 关闭这条规则  不能有空格
+    'space-before-function-paren': ['error', 'never']
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+// 导入通用的样式
+import './styles/base.less'
+import './styles/iconfont.css'
+// 导入vant
+import Vant from 'vant'
+// import { Button } from 'vant'
+import 'vant/lib/index.css'
+
+// 全局的把vant的所有组件都导入好了
+Vue.use(Vant)
+// Vue.use(Button)
+// import Button from 'vant/lib/button'
+// import 'vant/lib/button/style'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
